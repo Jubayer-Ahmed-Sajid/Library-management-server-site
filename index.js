c7d6b616562a0425ad566d9ab742a0e8a487a42b
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-11-06 14:36:29 +0600

    add book operation finished

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
+app.use(express.json());
+app.use(cors())
 const port = process.env.PORT || 5000;
 
 
@@ -20,19 +22,52 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
+        const database = client.db("Categories");
+        const CategoriesCollection = database.collection("categories");
+        const BooksDB = client.db("BooksDB");
+        const BooksCollection = BooksDB.collection("Books");
+
+        // get function
+        app.get('/', async (req, res) => {
+            res.send('Book library Server is running')
+        })
+        app.get('/categories', async(req,res)=>{
+            const result = await CategoriesCollection.find().toArray()
+            res.send(result)
+             
+
+        })
+        app.get('/allBooks', async(req,res)=>{
+            const result = await BooksCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/allBooks/:category', async(req,res)=>{
+            const category = req.params.category
+            const query = {category_name: category}
+            console.log(query)
+            const result = await BooksCollection.find(query).toArray()
+            res.send(result)
+        })
+        // post function
+        app.post('/allBooks', async(req,res)=>{
+            const book = req.body;
+            console.log(book)
+            const result = await BooksCollection.insertOne(book)
+            res.send (result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
-app.get('/', async (req, res) => {
-    res.send('Book library Server is running')
-})
+
 app.listen(port, console.log('server is running'))
